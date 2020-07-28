52964e4bceb026308a6d350f6acc5490f631fd13
ThuWD Kanye <61614127+ThuWD@users.noreply.github.com>
2020-07-28 15:39:10 +1000

    Update apps.sh

--- apps/apps.sh
+++ apps/apps.sh
@@ -1,6 +1,1 @@
-mkdir ~/Downloads/Apps/VSCode/
-cd ~/Downloads/Apps/VSCode/
-wget -O vscode.deb http://go.microsoft.com/fwlink/?LinkID=760868
-mkdir ~/Downloads/Apps/GHex/
-cd ~/Downloads/Apps/GHex/
-wget -O GHex.deb http://ubuntu.com/ubuntu/pool/universe/g/ghex/ghex_3.18.3-3_amd64.deb
+
