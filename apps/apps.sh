6ba61547dd02ff9815e55d6ee8d69fbf63a48075
ThuWD Kanye <61614127+ThuWD@users.noreply.github.com>
2020-07-28 15:23:26 +1000

    Create apps.sh

---
+++ apps/apps.sh
@@ -0,0 +1,6 @@
+mkdir ~/Downloads/Apps/VSCode/
+cd ~/Downloads/Apps/VSCode/
+wget -O vscode.deb https://go.microsoft.com/fwlink/?LinkID=760868
+mkdir ~/Downloads/Apps/GHex/
+cd ~/Downloads/Apps/GHex/
+wget -O GHex.deb https://ubuntu.com/ubuntu/pool/universe/g/ghex/ghex_3.18.3-3_amd64.deb
