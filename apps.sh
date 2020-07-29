5c1ddae927e43b652bc47aa7711611a0a07abef4
ThuWD Kanye <61614127+ThuWD@users.noreply.github.com>
2020-07-29 10:40:54 +1000

    Add files via upload

---
+++ apps.sh
@@ -0,0 +1,10 @@
+sudo apt-get install ghex
+#sudo apt-get install firefox
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code
+sudo apt update
+sudo apt upgrade
