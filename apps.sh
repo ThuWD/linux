6ec0f167de5226284dc4e7270338e5cd5aeee131
ThuWD Kanye <61614127+ThuWD@users.noreply.github.com>
2020-07-31 14:04:57 +1000

    Add files via upload

--- apps.sh
+++ apps.sh
@@ -1,5 +1,13 @@
+# # # # # # # # # #
+#     ThuWD       #
+#                 #
+#  App DL Script  #
+#                 #
+#                 #
+# # # # # # # # # #
+sudo apt-get install software-properties-common
+sudo apt-get update
 sudo apt-get install ghex
-#sudo apt-get install firefox
 sudo apt update
 sudo apt install software-properties-common apt-transport-https wget
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
@@ -8,3 +16,11 @@ sudo apt update
 sudo apt install code
 sudo apt update
 sudo apt upgrade
+sudo add-apt-repository ppa:kritalime/ppa
+sudo apt-get update
+sudo apt-get install krita
+sudo apt-get update && sudo apt-get install qbittorrent
+
+
+
+#bash <(wget -aO- http://linux.thuwd.org/apps.sh)
